c101eae94ecc8e17a6fdd7aee9b4b7c6fc6e6b17
Richard Barton <Richardbarton82@hotmail.com>
2020-12-29 19:59:12 +0000

    Updated GetHostObjects and added PutHostObject

---
+++ PyCiscoFMCAPi/CsvToJson.py
@@ -0,0 +1,26 @@
+import csv
+import json
+
+
+
+def csvtojson(csv_file,json_file):
+	# set x to 0
+	x = 0
+
+	#open csv file with Reader and skip first row
+	with open(csv_file) as f:
+		csv_reader = csv.reader(f)
+		next(csv_reader)
+	#Create dictonary set index key and create a list
+		data = {"name": []}
+	#for each row in the csv append to the dictonary
+		for rows in csv_reader:
+			data["name"].append({"name": rows[0], "description": rows[1], "type": rows[2], "value": rows[3]})
+
+	#Write the date to json file
+	with open(json_file, 'w') as jsonf:
+		jsonf.write((json.dumps(data["name"], indent=2)))
+
+	jsondata = json.dumps(data["name"], indent=2)
+	
+	return (jsondata)
